199594b5fc084a35f1b0c4709571a30eca5253b4
Tri Nguyen <trihuynguyen14@gmail.com>
2019-06-11 15:20:45 -0500

    changing back to jdbc :(

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.U1M6Summative.dao;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceItemRepository {
-}
--- U1M6Summative/src/main/java/com/company/U1M6Summative/model/Invoice.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/model/Invoice.java
@@ -1,12 +1,4 @@
 package com.company.U1M6Summative.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-@Table(name="invoice")
 public class Invoice {
 }
