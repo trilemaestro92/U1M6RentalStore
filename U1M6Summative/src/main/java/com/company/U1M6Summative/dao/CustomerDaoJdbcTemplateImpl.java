02b0967e3cdbf8c0d209f3cb523320063d2af5b9
zhakina90 <zhakina90@gmail.com>
2019-06-13 14:18:40 -0500

    Test for customer passed

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDaoJdbcTemplateImpl.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDaoJdbcTemplateImpl.java
@@ -1,13 +1,18 @@
 package com.company.U1M6Summative.dao;
 
 import com.company.U1M6Summative.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-    @Repository
-    public class CustomerDaoJdbcTemplateImpl implements CustomerDao{
+@Repository
+public class CustomerDaoJdbcTemplateImpl implements CustomerDao{
         private JdbcTemplate jdbcTemplate;
         private static final String INSERT_CUSTOMER_SQL =
                 "insert into customer (first_name, last_name, email, company, phone) values (?, ? , ?, ?, ?)";
@@ -25,31 +30,67 @@ import java.util.List;
 //    email varchar(75) not null,
 //    company varchar(50) not null,
 //    phone varchar(50) not null
+
+        @Autowired
+        public CustomerDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate){
+
+            this.jdbcTemplate = jdbcTemplate;
+        }
         @Override
+        @Transactional
         public Customer addCustomer(Customer customer) {
-            return null;
+jdbcTemplate.update(INSERT_CUSTOMER_SQL,
+        customer.getFirst_name(),
+        customer.getLast_name(),
+        customer.getEmail(),
+        customer.getCompany(),
+        customer.getPhone());
+int id =jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class);
+customer.setCustomer_id(id);
+            return  customer;
         }
 
         @Override
         public Customer getCustomer(int id) {
-            return null;
+            try{
+                return  jdbcTemplate.queryForObject(SELECT_CUSTOMER_SQL, this::mapRowToCustomer, id);
+            }catch (EmptyResultDataAccessException e){
+
+                return null;
+            }
         }
 
         @Override
         public List<Customer> getAllCustomers() {
-            return null;
+            return jdbcTemplate.query(SELECT_ALL_CUSTOMER_SQL, this::mapRowToCustomer);
         }
 
         @Override
-        public void updateCustomer() {
-
+        public void updateCustomer(Customer customer) {
+    jdbcTemplate.update(UPDATE_CUSTOMER_SQL,
+        customer.getFirst_name(),
+        customer.getLast_name(),
+        customer.getEmail(),
+        customer.getCompany(),
+        customer.getPhone(),
+        customer.getCustomer_id());
+//return customer;
         }
 
         @Override
         public void deleteCustomer(int id) {
-
+jdbcTemplate.update(DELETE_SQL, id);
         }
 
-
+    private Customer mapRowToCustomer(ResultSet rs, int rowNum) throws SQLException{
+        Customer customer = new Customer();
+        customer.setCustomer_id(rs.getInt("customer_id"));
+        customer.setFirst_name(rs.getString("first_name"));
+        customer.setLast_name(rs.getString("last_name"));
+        customer.setEmail(rs.getString("email"));
+        customer.setCompany(rs.getString("company"));
+        customer.setPhone(rs.getString("phone"));
+        return customer;
+    }
 
 }
