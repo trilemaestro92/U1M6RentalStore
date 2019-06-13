aa79d1d2fe0d98e625c735736c3fd3d98bac0fd7
tanz <contact@johntansor.dev>
2019-06-13 10:10:45 -0500

    adds invoiceDaoimplementation & test template

---
+++ U1M6Summative/src/test/java/com/company/U1M6Summative/dao/InvoiceDaoJbdcTemplateImplTest.java
@@ -0,0 +1,25 @@
+package com.company.U1M6Summative.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InvoiceDaoJbdcTemplateImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addInvoice() {
+    }
+
+    @Test
+    public void deleteInvoice() {
+    }
+
+    @Test
+    public void findInvoiceByCustomer() {
+    }
+}
