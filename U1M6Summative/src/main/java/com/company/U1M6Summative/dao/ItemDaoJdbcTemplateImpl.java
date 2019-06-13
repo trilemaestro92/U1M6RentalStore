d8a2c0c9353bf36cb4ef2f8feac840ddceded527
Luis <ls8salmeron@yahoo.com>
2019-06-13 14:25:33 -0500

    Testing for Invoice Items

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/ItemDaoJdbcTemplateImpl.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/ItemDaoJdbcTemplateImpl.java
@@ -4,12 +4,14 @@ import com.company.U1M6Summative.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class ItemDaoJdbcTemplateImpl implements ItemDao {
 
     private JdbcTemplate jdbcTemplate;
--- U1M6Summative/src/test/java/com/company/U1M6Summative/dao/InvoiceItemDaoTest.java
+++ U1M6Summative/src/test/java/com/company/U1M6Summative/dao/InvoiceItemDaoTest.java
@@ -1,26 +1,72 @@
 package com.company.U1M6Summative.dao;
 
+import com.company.U1M6Summative.model.Invoice;
+import com.company.U1M6Summative.model.InvoiceItem;
+import com.company.U1M6Summative.model.Item;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class InvoiceItemDaoTest {
+
+    @Autowired
+    InvoiceItemDao invoiceItemDao;
     @Autowired
     CustomerDao customerDao;
     @Autowired
     InvoiceDao invoiceDao;
-    @Autowired
-    InvoiceItemDao invoiceItemDao;
     @Autowired
     ItemDao itemDao;
 
 
     @Before
     public void setUp() throws Exception {
+        List<InvoiceItem> invoiceItemList = invoiceItemDao.getAllInvoiceItems();
+        for (InvoiceItem i : invoiceItemList) {
+            invoiceItemDao.deleteInvoiceItem(i.getInvoiceItemId());
+        }
+
+    }
+
+    @Test
+    public void addGetDeleteInvoiceItem(){
+        Item testItem = new Item("Computer","A test computer",new BigDecimal("10.00"));
+
+        Invoice testInvoice = new Invoice(22, LocalDate.of(2019,12,19),
+                LocalDate.of(2019, 12, 22), LocalDate.of(2020, 2, 19),
+                new BigDecimal(30.00));
+
+        testItem = itemDao.addItem(testItem);
+        testInvoice = invoiceDao.addInvoice(testInvoice);
+
+        InvoiceItem invoiceItem1 = new InvoiceItem
+                (testInvoice.getInvoice_id(),
+                testItem.getItem_id(),
+                1,
+                new BigDecimal(50.00), new BigDecimal(15.00));
+
+
+
+        invoiceItem1 = invoiceItemDao.addInvoiceItem(invoiceItem1);
+        InvoiceItem invoiceItem2 = invoiceItemDao.getInvoiceItem(invoiceItem1.getInvoiceItemId());
+
+        assertEquals(invoiceItem1, invoiceItem2);
+
+        invoiceItemDao.deleteInvoiceItem(invoiceItem1.getInvoiceItemId());
+
+        invoiceItem2 = invoiceItemDao.getInvoiceItem(invoiceItem1.getInvoiceItemId());
+
+        assertNull(invoiceItem2);
 
     }
 
