5527da516c1b569b85f90d0bd19be109b2091798
Luis <ls8salmeron@yahoo.com>
2019-06-14 14:48:30 -0500

    finish all item mock test and item service layer

--- U1M6Summative/src/main/java/com/company/U1M6Summative/viewModel/InvoiceItemViewModel.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/viewModel/InvoiceItemViewModel.java
@@ -3,23 +3,24 @@ package com.company.U1M6Summative.viewModel;
 import com.company.U1M6Summative.model.Invoice;
 import com.company.U1M6Summative.model.Item;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 public class InvoiceItemViewModel {
 
     private int invoiceItemId;
-    private Invoice invoiceId;
-    private Item itemId;
+    private Invoice invoice;
+    private Item item;
     private int quantity;
-    private double unitRate;
-    private double discount;
+    private BigDecimal unitRate;
+    private BigDecimal discount;
 
     public InvoiceItemViewModel(){
         super();
     }
-    public InvoiceItemViewModel(Invoice invoiceId, Item itemId, int quantity, double unitRate, double discount) {
-        this.invoiceId = invoiceId;
-        this.itemId = itemId;
+    public InvoiceItemViewModel(Invoice invoice, Item item, int quantity, BigDecimal unitRate, BigDecimal discount) {
+        this.invoice = invoice;
+        this.item = item;
         this.quantity = quantity;
         this.unitRate = unitRate;
         this.discount = discount;
@@ -33,20 +34,20 @@ public class InvoiceItemViewModel {
         this.invoiceItemId = invoiceItemId;
     }
 
-    public Invoice getInvoiceId() {
-        return invoiceId;
+    public Invoice getInvoice() {
+        return invoice;
     }
 
-    public void setInvoiceId(Invoice invoiceId) {
-        this.invoiceId = invoiceId;
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
     }
 
-    public Item getItemId() {
-        return itemId;
+    public Item getItem() {
+        return item;
     }
 
-    public void setItemId(Item itemId) {
-        this.itemId = itemId;
+    public void setItem(Item item) {
+        this.item = item;
     }
 
     public int getQuantity() {
@@ -57,19 +58,19 @@ public class InvoiceItemViewModel {
         this.quantity = quantity;
     }
 
-    public double getUnitRate() {
+    public BigDecimal getUnitRate() {
         return unitRate;
     }
 
-    public void setUnitRate(double unitRate) {
+    public void setUnitRate(BigDecimal unitRate) {
         this.unitRate = unitRate;
     }
 
-    public double getDiscount() {
+    public BigDecimal getDiscount() {
         return discount;
     }
 
-    public void setDiscount(double discount) {
+    public void setDiscount(BigDecimal discount) {
         this.discount = discount;
     }
 
@@ -80,14 +81,14 @@ public class InvoiceItemViewModel {
         InvoiceItemViewModel that = (InvoiceItemViewModel) o;
         return invoiceItemId == that.invoiceItemId &&
                 quantity == that.quantity &&
-                Double.compare(that.unitRate, unitRate) == 0 &&
-                Double.compare(that.discount, discount) == 0 &&
-                invoiceId.equals(that.invoiceId) &&
-                itemId.equals(that.itemId);
+                invoice.equals(that.invoice) &&
+                item.equals(that.item) &&
+                unitRate.equals(that.unitRate) &&
+                discount.equals(that.discount);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(invoiceItemId, invoiceId, itemId, quantity, unitRate, discount);
+        return Objects.hash(invoiceItemId, invoice, item, quantity, unitRate, discount);
     }
 }
